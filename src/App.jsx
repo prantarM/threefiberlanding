c324a0feb90d1f665095e2ec77ff9290d040735a
prantarM <prantar.misra@gmail.com>
2024-07-22 13:08:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LandingPage from "./assets/components/LandingPage.jsx"
+function App() {
+  
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/LandingPage.jsx
@@ -0,0 +1,112 @@
+import React, { useRef } from "react";
+import { OrbitControls, Sphere } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {pointsInner,pointsOuter} from '../../../utils/util.js';
+
+export default function LandingPage() {
+
+    return (
+  
+      <div className="relative">
+  
+        <Canvas
+  
+          camera={{
+  
+            position: [10, -7.5, -5],
+  
+          }}
+  
+          style={{ height: "100vh" }}
+  
+          className="bg-slate-900"
+  
+        >
+  
+          <OrbitControls maxDistance={20} minDistance={10} />
+  
+          <directionalLight />
+  
+          <pointLight position={[-30, 0, -30]} power={10.0} />
+  
+          <PointCircle />
+  
+        </Canvas>
+  
+  
+        <h1 className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-slate-200 font-extrabold text-2xl md:text-5xl pointer-events-none shadow-2xl">
+  
+          Landing Page
+  
+        </h1>
+  
+      </div>
+  
+    );
+  
+  };
+  
+  
+  const PointCircle = () => {
+  
+    const ref = useRef(null);
+  
+  
+    useFrame(({ clock }) => {
+  
+      if (ref.current?.rotation) {
+  
+        ref.current.rotation.z = clock.getElapsedTime() * 0.05;
+  
+      }
+  
+    });
+  
+  
+    return (
+  
+      <group ref={ref}>
+  
+        {pointsInner.map((point) => (
+  
+          <Point key={point.idx} position={point.position} color={point.color} />
+  
+        ))}
+  
+        {pointsOuter.map((point) => (
+  
+          <Point key={point.idx} position={point.position} color={point.color} />
+  
+        ))}
+  
+      </group>
+  
+    );
+  
+  };
+  
+  
+  const Point = ({ position, color }) => {
+  
+    return (
+  
+      <Sphere position={position} args={[0.1, 10, 10]}>
+  
+        <meshStandardMaterial
+  
+          emissive={color}
+  
+          emissiveIntensity={0.5}
+  
+          roughness={0.5}
+  
+          color={color}
+  
+        />
+  
+      </Sphere>
+  
+    );
+  
+  };
+  
